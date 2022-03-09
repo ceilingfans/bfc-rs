91d003f370ca43c2ddd123f75259bd99af83625e
awned <65061212+ceilingfans@users.noreply.github.com>
2022-03-09 20:24:20 +0800

    feat(AST): parse function

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,5 @@
-use std::string::ParseError;
+use std::slice::SliceIndex;
+use self::Node::*;
 
 #[derive(Debug, PartialEq)]
 pub struct Location {
@@ -26,4 +27,98 @@ pub enum Node {
         body: Vec<Node>,
         loc: Option<Location>,
     },
+}
+
+#[derive(Debug)]
+pub struct ParserError {
+    pub message: String,
+    pub loc: Location,
+}
+
+pub fn parse(source: &str) -> Result<Vec<Node>, ParserError> {
+    let mut instructions = vec![];
+    let mut stack = vec![];
+
+    for (index, c) in source.chars().enumerate() {
+        match c {
+            '+' => instructions.push(CellShift {
+                amount: 1,
+                loc: Some(Location {
+                    start: index,
+                    end: index,
+                })
+            }),
+            '-' => instructions.push(CellShift {
+                amount: -1,
+                loc: Some(Location {
+                    start: index,
+                    end: index,
+                })
+            }),
+            '>' => instructions.push(PointerShift {
+                amount: 1,
+                loc: Some(Location {
+                    start: index,
+                    end: index,
+                })
+            }),
+            '<' => instructions.push(PointerShift {
+                amount: -1,
+                loc: Some(Location {
+                    start: index,
+                    end: index,
+                })
+            }),
+            '.' => instructions.push(Write {
+                loc: Some(Location {
+                    start: index,
+                    end: index,
+                })
+            }),
+            ',' => instructions.push(Read {
+                loc: Some(Location {
+                    start: index,
+                    end: index,
+                })
+            }),
+            '[' => {
+                stack.push((instructions, index));
+                instructions = vec![];
+            },
+            ']' => {
+                if let Some((mut parent, open_index)) = stack.pop() {
+                    parent.push(Loop {
+                        body: instructions,
+                        loc: Some(Location {
+                            start: open_index,
+                            end: index,
+                        })
+                    });
+                    instructions = parent;
+                } else {
+                    return Err(ParserError {
+                        message: "Unmatched bracket pair".to_owned(),
+                        loc: Location {
+                            start: index,
+                            end: index,
+                        }
+                    });
+                }
+            }
+            _ => {},
+        }
+    }
+
+    if !stack.is_empty() {
+        let pos = stack.last().unwrap().1;
+        return Err(ParserError {
+            message: "Unmatched bracket pair".to_owned(),
+            loc: Location {
+                start: pos,
+                end: pos,
+            }
+        })
+    }
+
+    Ok(instructions)
 }
